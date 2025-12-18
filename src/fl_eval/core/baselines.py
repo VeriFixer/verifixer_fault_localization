62fe412fa914dcc736ca7be03b93005c010f9e0f
ricostynha <amfpsilva@hotmail.com>
2025-12-18 17:59:30 +0000

    Added first counter example finder implementation

--- src/fl_eval/core/baselines.py
+++ src/fl_eval/core/baselines.py
@@ -13,4 +13,19 @@ class RandomRanker(FLTechnique):
         line_numbers = list(range(1, len(lines) + 1))
         random.shuffle(line_numbers)
         return line_numbers
-    
+
+import os
+class CounterExampleBaseRanker(FLTechnique):
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__(name, **kwargs)
+
+    def get_fault_localization(self, file: Path) -> list[int]:
+        DAFNY_EXEC = os.environ.get("DAFNY_EXEC")
+        assert (DAFNY_EXEC != None), "an environmental variable DFANY_EXEC must be set to dafny binary path"
+
+        with open(file, "r") as f:
+            lines = f.readlines()
+
+        line_numbers = list(range(1, len(lines) + 1))
+        random.shuffle(line_numbers)
+        return line_numbers
